2361e22186903d1c40c9fbe7c0b04704c4cb0f36
Luke Johnson <LukeRJohnsonUT@gmail.com>
2025-05-18 15:22:43 -0600

    started on server

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -3,6 +3,7 @@ import chess.*;
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        var port = 2020;
         System.out.println("♕ 240 Chess Server: " + piece);
     }
 }
