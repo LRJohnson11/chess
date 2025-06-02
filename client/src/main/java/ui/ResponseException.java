78a38f7486ad4e0fdc671accd45eb529e05a1d47
Luke Johnson <LukeRJohnsonUT@gmail.com>
2025-06-02 09:40:52 -0600

    updated response exception

---
+++ client/src/main/java/ui/ResponseException.java
@@ -0,0 +1,38 @@
+package ui;
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResponseException extends Exception{
+    private final int statusCode;
+
+    public ResponseException(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public ResponseException(String message, int statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+
+    public String toJson(){
+        return new Gson().toJson(Map.of("message", getMessage(), "status", statusCode));
+    }
+
+    public static ResponseException fromJson(InputStream stream) {
+        var map = new Gson().fromJson(new InputStreamReader(stream), HashMap.class);
+        var status = ((Double)map.get("status")).intValue();
+        String message = map.get("message").toString();
+        return new ResponseException(message,status);
+    }
+
+}
